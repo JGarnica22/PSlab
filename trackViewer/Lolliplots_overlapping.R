c07e5053b4b53097762c3f463b3988cc5a8bccff
PatriSolé <52314052+patriciasolesanchez@users.noreply.github.com>
2020-05-18 08:51:57 +0200

    Update Lolliplots_overlapping.R

--- trackViewer/Lolliplots_overlapping.R
+++ trackViewer/Lolliplots_overlapping.R
@@ -1,7 +1,8 @@
-#This script is to visualize methylome data by trackViewer doing lolliplots
+#This script is to visualize single C methylation data by trackViewer
+#in lolliplots (or lollipop plots)
 #It was created for R 3.6.3 version (2020-05-03)
-#Copyright (C) 2020  Patricia Sole Sanchez
-##########################################################
+#Copyright (C) 2020  Patricia Solé Sánchez and Josep Garnica Caparrós
+######################################################################
 # Check if required packages are installed, if not install:
 cran.packages <- c("Cairo", "stringr", "tidyr")
 for (i in cran.packages) {
@@ -43,8 +44,7 @@ setwd("/home/jgarnica/R/trackviewer")
 
 # Indicate populations to work with, first indicate control population.
 pop <- c("Tconv", "Tet")
-
-#Inidicate control and sample names
+# Indicate control and sample names
 comp <- c("Ctl", "Smp")
 
 # Indicate species working with (mouse or human)
@@ -64,54 +64,54 @@ if (species == "mouse"){
   org.SYMBOL <- org.Hs.egSYMBOL
 }
 
-#Import file with methylation information for each nucleotid comparing the two populations to compare
+# Import file with methylation information for each nucleotid comparing the two populations to compare
 file_list_txt <- list.files(path="/home/jgarnica/R/trackviewer/data",pattern= "CG.")
 
 for (file in c(file_list_txt)){
   x <- read.table(paste0("data/", file), sep = "\t", dec = ".", header = T, quote = "")
   for (u in 1:length(comp)){
-  y <- GRanges(seqnames = x$chr, 
-               ranges = IRanges(start = x$pos,
-                                width = 1), 
-               strand = NULL,
-               #We cross here columns because we assume groups were crossed
-               #in the analysis!!
-               score = as.integer(x[,u+2]*100))
-  assign(paste0("lolli.", comp[u]), y)
-    }
-  
-
-#Lolliplots overlapping samples:
-#Call all GRanges objects to plot
-grl <- c(mget(grep("lolli*",names(.GlobalEnv),value=TRUE)))
-#set colors to use
-colors <- rainbow(length(pop))
-pdf(paste0("figs/Lolliplot_", paste0(pop, collapse ="_"), ".pdf"), width = 12, height = 6)
-for (plot in plots) {
-  id <- get(plot, org.SYMBOL2EG)
-  gr <- genes(TxDb)[id]
-  gene <- geneTrack(id,TxDb)[[1]]$dat
-  gene$fill <- "lightblue"
-  gene$height <- 0.03
-  grlo <- GRanges()
-  for (p in 1:length(grl)){
-    grl[[p]]$color <- colors[p]
-    grl[[p]]$border <- "gray30"
-    grlo <- c(grlo, grl[[p]])
+    y <- GRanges(seqnames = x$chr, 
+                 ranges = IRanges(start = x$pos,
+                                  width = 1), 
+                 strand = NULL,
+                 #We cross here columns because we assume groups were crossed
+                 #in the analysis!!
+                 score = as.integer(x[,u+2]*100))
+    assign(paste0("lolli.", comp[u]), y)
   }
-  xaxis <- c(seq(from = start(gr), 
-                 to = end(gr), 
-                 by = 500))
-  yaxis <- c(0, 100)
-  legends <- list(list(labels=pop, 
-                       fill= colors,
-                       col = c("gray30","gray30")))
   
-  lolliplot(grlo, gene, type = "circle", 
-            xaxis= xaxis,
-            yaxis= yaxis,
-            legend= legends,
-            ylab= plot)
+  
+  #Lolliplots overlapping samples:
+  #Call all GRanges objects to plot
+  grl <- c(mget(grep("lolli*",names(.GlobalEnv),value=TRUE)))
+  #set colors to use
+  colors <- rainbow(length(pop))
+  pdf(paste0("figs/Lolliplot_", paste0(pop, collapse ="_"), ".pdf"), width = 12, height = 6)
+  for (plot in plots) {
+    id <- get(plot, org.SYMBOL2EG)
+    gr <- genes(TxDb)[id]
+    gene <- geneTrack(id,TxDb)[[1]]$dat
+    gene$fill <- "lightblue"
+    gene$height <- 0.03
+    grlo <- GRanges()
+    for (p in 1:length(grl)){
+      grl[[p]]$color <- colors[p]
+      grl[[p]]$border <- "gray30"
+      grlo <- c(grlo, grl[[p]])
     }
-dev.off()
+    xaxis <- c(seq(from = start(gr), 
+                   to = end(gr), 
+                   by = 500))
+    yaxis <- c(0, 100)
+    legends <- list(list(labels=pop, 
+                         fill= colors,
+                         col = c("gray30","gray30")))
+    
+    lolliplot(grlo, gene, type = "circle", 
+              xaxis= xaxis,
+              yaxis= yaxis,
+              legend= legends,
+              ylab= plot)
+  }
+  dev.off()
 }
