a999a3a2553e670c2eda44fbf06d3914ff396ec6
Josep Garnica <61703467+JGarnica22@users.noreply.github.com>
2020-05-18 14:08:34 +0200

    Update Chromosome_views.R
    naming of all names solved

--- trackViewer/Chromosome_views.R
+++ trackViewer/Chromosome_views.R
@@ -38,7 +38,7 @@ library(stringr)
 library(Cairo)
 
 # Set your working directory (the project you are working in):
-setwd("/Users/patri/Documents/LAB/TESIS DOCTORAL 2015-2020/INTERNSHIP/8_trackViewer")
+setwd("/home/jgarnica/R/trackviewer")
 
 # Indicate populations to work with
 pop <- c("Tconv", "Tet")
@@ -47,7 +47,7 @@ pop <- c("Tconv", "Tet")
 species <- "mouse"
 
 # Indicate the genes to visualize
-plots <- c("Il10", "Il21")
+plots <- c("Il10", "Il21", "Mapkapk2", "Il19" )
 # or read a txt file containing a list with all the plots
 
 # Create functions to be used:
@@ -72,24 +72,29 @@ if (species == "mouse"){
 for (xy in plots) {
   id <- get(xy, org.SYMBOL2EG)
   gr <- genes(TxDb)[id]
-  agr <- if (width(gr)<100000) {
+  agr <- if (width(gr)<120000) {
     resize(gr, 100000, fix = "center")
   } else {
     resize(gr, width(gr)+20000, fix = "center")
   }
-  genesinrange <- mapRangesToIds(TxDb, agr, type = "gene")
+  # agr.gene <- if (width(gr)<100000) {
+  #   resize(gr, 300000, fix = "center")
+  # } else {
+  #   resize(gr, width(gr)+50000, fix = "center")
+  # }
+  genesinrange <- mapRangesToIds(TxDb, agr, type = "gene", ignore.strand = T)
   tracks <- sapply(genesinrange[[1]][[1]], 
                    function(z) {
                      track <- geneTrack(z,TxDb)[[1]]
                      return(track)
                    })
-  
+ 
   #Import results files from the different analysis to be included and for each replicate:
   #List all the files in you data directory and import them depending on their format
   #IMPORTANT: name files always as type_of_sample(Tet, Tconv, TFH...)+number of replicate_tecnique (ATAC, RNA...), e.g. Tet3_ATAC, Tet1_RNA...
-  file_list_bed <- list.files(path="/Users/patri/Documents/LAB/TESIS DOCTORAL 2015-2020/INTERNSHIP/8_trackViewer/data",
+  file_list_bed <- list.files(path="/home/jgarnica/R/trackviewer/data",
                               pattern= "*.bed")
-  file_list_bw <- list.files(path="/Users/patri/Documents/LAB/TESIS DOCTORAL 2015-2020/INTERNSHIP/8_trackViewer/data",
+  file_list_bw <- list.files(path="/home/jgarnica/R/trackviewer/data",
                              pattern= "*.bw")
   
   for (file in c(file_list_bed)){
@@ -119,171 +124,178 @@ for (xy in plots) {
         assign(paste0(s, ".", comp[l], i), me)
       }
     }
-
-  #indicate experiments with multiple replicates
-  for (n in c("ATAC", "RNA")){
-    #assign all objects to be used for operations
-    for(w in 1:length(grep(paste0(n, "*.",comp[l], "."), names(.GlobalEnv),value=TRUE))){
-      a <- eval(as.symbol(grep(paste0(n, ".",comp[l], "."), names(.GlobalEnv),value=TRUE)[w]))
-      assign(paste0("mit",w), a)
+    
+    #indicate experiments with multiple replicates
+    for (n in c("ATAC", "RNA")){
+      #assign all objects to be used for operations
+      for(w in 1:length(grep(paste0(n, "*.",comp[l], "."), names(.GlobalEnv),value=TRUE))){
+        a <- eval(as.symbol(grep(paste0(n, ".",comp[l], "."), names(.GlobalEnv),value=TRUE)[w]))
+        assign(paste0("mit",w), a)
       }
-    
-    if (length(grep("mit", names(.GlobalEnv),value=TRUE)) == 2) {
-          avr <- mit1
-          avr$dat <- avr$dat
-          avr$dat2 <- mit2$dat
-          avr$dat <- GRoperator(avr$dat, avr$dat2, 
-                                col="score", operator=average)
-          avr$dat2 <- GRanges()
-          assign(paste0("arch", n, ".", comp[l]), avr)
-          }
-    
-    if (length(grep("mit", names(.GlobalEnv),value=TRUE)) == 3){
-      avr <- mit1
-      avr$dat <- avr$dat
-      avr$dat2 <- mit2$dat
-      avr$dat <- GRoperator(avr$dat, avr$dat2, 
-                            col="score", operator="+")
-      avr$dat2 <- mit3$dat
-      avr$dat <- GRoperator(avr$dat, avr$dat2, 
-                             col="score", operator=average3)
-      avr$dat2 <- GRanges()
-      assign(paste0("arch", n, ".", comp[l]), avr)
-      }
-    
-    if (length(grep("mit", names(.GlobalEnv),value=TRUE)) == 4){
-      avrg <- mit1
-      avrg$dat <- avrg$dat
-      avrg$dat2 <- mit2$dat
-      avrg$dat <- GRoperator(avrg$dat, avrg$dat2, 
-                             col="score", operator=average)
-      avrg$dat2 <- GRanges()
       
-      avrg2 <- mit3
-      avrg2$dat <- avrg2$dat
-      avrg2$dat2 <- mit4$dat
-      avrg2$dat <- GRoperator(avrg2$dat, avrg2$dat2, 
+      if (length(grep("mit", names(.GlobalEnv),value=TRUE)) == 2) {
+        avr <- mit1
+        avr$dat <- avr$dat
+        avr$dat2 <- mit2$dat
+        avr$dat <- GRoperator(avr$dat, avr$dat2, 
                               col="score", operator=average)
-      avrg2$dat2 <- GRanges()
+        avr$dat2 <- GRanges()
+        assign(paste0("arch", n, ".", comp[l]), avr)
+      }
       
-      avr <- avrg
-      avr$dat <- avr$dat
-      avr$dat2 <- avrg2$dat
-      avr$dat <- GRoperator(avr$dat, avr$dat2, 
-                            col="score", operator=average)
-      avr$dat2 <- GRanges()
-      assign(paste0("arch", n, ".", comp[l]), avr)
+      if (length(grep("mit", names(.GlobalEnv),value=TRUE)) == 3){
+        avr <- mit1
+        avr$dat <- avr$dat
+        avr$dat2 <- mit2$dat
+        avr$dat <- GRoperator(avr$dat, avr$dat2, 
+                              col="score", operator="+")
+        avr$dat2 <- mit3$dat
+        avr$dat <- GRoperator(avr$dat, avr$dat2, 
+                              col="score", operator=average3)
+        avr$dat2 <- GRanges()
+        assign(paste0("arch", n, ".", comp[l]), avr)
+      }
+      
+      if (length(grep("mit", names(.GlobalEnv),value=TRUE)) == 4){
+        avrg <- mit1
+        avrg$dat <- avrg$dat
+        avrg$dat2 <- mit2$dat
+        avrg$dat <- GRoperator(avrg$dat, avrg$dat2, 
+                               col="score", operator=average)
+        avrg$dat2 <- GRanges()
+        
+        avrg2 <- mit3
+        avrg2$dat <- avrg2$dat
+        avrg2$dat2 <- mit4$dat
+        avrg2$dat <- GRoperator(avrg2$dat, avrg2$dat2, 
+                                col="score", operator=average)
+        avrg2$dat2 <- GRanges()
+        
+        avr <- avrg
+        avr$dat <- avr$dat
+        avr$dat2 <- avrg2$dat
+        avr$dat <- GRoperator(avr$dat, avr$dat2, 
+                              col="score", operator=average)
+        avr$dat2 <- GRanges()
+        assign(paste0("arch", n, ".", comp[l]), avr)
+      }
+      rm(list=grep("mit", names(.GlobalEnv), value = T))
     }
-    rm(list=grep("mit", names(.GlobalEnv), value = T))
   }
-}
-    
-
-# Create tracks for each group, add "arch" for replicates:
+  
+  
+  # Create tracks for each group, add "arch" for replicates:
   show.tracks <- c(mget(grep("methy.",names(.GlobalEnv),value=TRUE, fixed = T)),
                    mget(grep("enh.",names(.GlobalEnv),value=TRUE, fixed = T)),
                    mget(grep("ChIP.",names(.GlobalEnv),value=TRUE, fixed = T)),
                    mget(grep("archATAC.",names(.GlobalEnv),value=TRUE, fixed = T)),
                    mget(grep("archRNA.",names(.GlobalEnv),value=TRUE, fixed = T)))
-
-# Change the track names
-names(tracks) <- sapply(names(tracks), 
-                        function(z) {
-                          names <- get(z, org.Mm.egSYMBOL)
-                          return(names)
-                        })
-
-pdf(file = paste0("figs/",xy, ".pdf"), width = 10, height = 6)
-for (m in 1:length(pop)){
-  Pattern_list<-do.call("list",mget(grep(comp[m],names(show.tracks),value=TRUE)))
-  o <- optimizeStyle(trackList(Pattern_list, tracks))
-  assign(paste0("optSty", m), o)
-  u <- o$tracks
-  assign(paste0("trackList", m), t)
-  t <- o$style
-  assign(paste0("viewerStyle", m), u)
   
-  # Adjust X axis to show scale instead of chromosome ruler:
-  setTrackViewerStyleParam(t, "xaxis", FALSE)
-  setTrackViewerStyleParam(t, "margin", c(.01, .05, .01, .01))
+  # Change the track names
+  names(tracks) <- sapply(names(tracks), 
+                          function(z) {
+                            names <- get(z, org.Mm.egSYMBOL)
+                            return(names)
+                          })
   
-  setTrackXscaleParam(u[[1]], "draw", FALSE)
-  setTrackXscaleParam(u[[5]], "draw", TRUE) #REDUNDANT??
-  setTrackXscaleParam(u[[5]], "gp", list(cex=.7))
-  # Move scale in y axis to the right:
-  setTrackViewerStyleParam(t, "margin", c(.08, .15, .01, .1))
-  for(i in 1:5){
-    setTrackYaxisParam(u[[i]], "main", FALSE)
-  }
-  for(i in c(1,3:5)){
-    setTrackYaxisParam(u[[i]], "gp", list(cex=.9))
-  }
-  # Eliminate scale for Active enhancers track:
-  setTrackYaxisParam(u[[2]], "draw", FALSE)
-  ## Adjust the limit of y-axis for RNA, ATAC and ChIP tracks:
-  setTrackStyleParam(u[[1]], "ylim", c(0, 1))
-  setTrackStyleParam(u[[2]], "ylim", c(0, 1))
-  setTrackStyleParam(u[[3]], "ylim", c(0, roundUpNice(max(optimizeStyle(trackList(do.call("list",mget(grep(comp[1],names(show.tracks),value=TRUE))), tracks))$tracks[[3]]$dat$score,
-                                                          optimizeStyle(trackList(do.call("list",mget(grep(comp[2],names(show.tracks),value=TRUE))), tracks))$tracks[[3]]$dat$score))))
-  setTrackStyleParam(u[[4]], "ylim", c(0, roundUpNice(max(optimizeStyle(trackList(do.call("list",mget(grep(comp[1],names(show.tracks),value=TRUE))), tracks))$tracks[[4]]$dat$score,
-                                                          optimizeStyle(trackList(do.call("list",mget(grep(comp[2],names(show.tracks),value=TRUE))), tracks))$tracks[[4]]$dat$score))))
-  setTrackStyleParam(u[[5]], "ylim", c(0, roundUpNice(max(optimizeStyle(trackList(do.call("list",mget(grep(comp[1],names(show.tracks),value=TRUE))), tracks))$tracks[[5]]$dat$score,
-                                                          optimizeStyle(trackList(do.call("list",mget(grep(comp[2],names(show.tracks),value=TRUE))), tracks))$tracks[[5]]$dat$score))))
-  
-  for(i in 1:5){
-    setTrackStyleParam(u[[i]], "marginBottom", .1)
-  }
-  # For each transcript, the transcript name can be put on the upstream or downstream 
-  #of the transcript
-  for(i in 6:length(u)){
-    setTrackStyleParam(u[[i]], "ylabpos", "upstream")
-    setTrackStyleParam(u[[i]], "ylabgp", list(cex=1))
-  }
-  # Adjust the label of y-axis (colour and size)
-  for (i in 1:5){
-    setTrackStyleParam(u[[i]], "ylabgp", list(cex=.8, col="black")) 
-  }
-  # Adjust track colors:
-  setTrackStyleParam(u[[1]], "color", "darkgreen")
-  setTrackStyleParam(u[[2]], "color", "red")
-  setTrackStyleParam(u[[3]], "color", "red")
-  setTrackStyleParam(u[[4]], "color", "purple3")
-  setTrackStyleParam(u[[5]], "color", "darkblue")
-  for(i in 6:length(u)){
-    setTrackStyleParam(u[[i]], "color", "grey35")
-  }
-  
-  names(u) <- c("DIFF METH", "DIFF ACT ENH", "H3K27ac", 
-                "ATAC", "RNA", names(tracks))
-  
-  # Adjust the track height, add conditionals in case many genes are added
-  if (length(u) <= 10){
-    setTrackStyleParam(u[[2]], "height", 0.04)
+  pdf(file = paste0("figs/",xy, ".pdf"), width = 10, height = 6)
+  for (m in 1:length(pop)){
+    Pattern_list<-do.call("list",mget(grep(comp[m],names(show.tracks),value=TRUE)))
+    o <- optimizeStyle(trackList(Pattern_list, tracks))
+    assign(paste0("optSty", m), o)
+    u <- o$tracks
+    assign(paste0("trackList", m), t)
+    t <- o$style
+    assign(paste0("viewerStyle", m), u)
+    
+    # Adjust X axis to show scale instead of chromosome ruler:
+    setTrackViewerStyleParam(t, "xaxis", FALSE)
+    setTrackViewerStyleParam(t, "margin", c(.01, .05, .01, .01))
+    
+    setTrackXscaleParam(u[[1]], "draw", FALSE)
+    setTrackXscaleParam(u[[5]], "draw", TRUE) #REDUNDANT??
+    setTrackXscaleParam(u[[5]], "gp", list(cex=.7))
+    # Move scale in y axis to the right:
+    setTrackViewerStyleParam(t, "margin", c(.08, .15, .01, .1))
+    for(i in 1:5){
+      setTrackYaxisParam(u[[i]], "main", FALSE)
+    }
     for(i in c(1,3:5)){
-      setTrackStyleParam(u[[i]], "height", 0.170)
+      setTrackYaxisParam(u[[i]], "gp", list(cex=.9))
     }
+    # Eliminate scale for Active enhancers track:
+    setTrackYaxisParam(u[[2]], "draw", FALSE)
+    ## Adjust the limit of y-axis for RNA, ATAC and ChIP tracks:
+    setTrackStyleParam(u[[1]], "ylim", c(0, 1))
+    setTrackStyleParam(u[[2]], "ylim", c(0, 1))
+    setTrackStyleParam(u[[3]], "ylim", c(0, roundUpNice(max(optimizeStyle(trackList(do.call("list",mget(grep(comp[1],names(show.tracks),value=TRUE))), tracks))$tracks[[3]]$dat$score,
+                                                            optimizeStyle(trackList(do.call("list",mget(grep(comp[2],names(show.tracks),value=TRUE))), tracks))$tracks[[3]]$dat$score))))
+    setTrackStyleParam(u[[4]], "ylim", c(0, roundUpNice(max(optimizeStyle(trackList(do.call("list",mget(grep(comp[1],names(show.tracks),value=TRUE))), tracks))$tracks[[4]]$dat$score,
+                                                            optimizeStyle(trackList(do.call("list",mget(grep(comp[2],names(show.tracks),value=TRUE))), tracks))$tracks[[4]]$dat$score))))
+    setTrackStyleParam(u[[5]], "ylim", c(0, roundUpNice(max(optimizeStyle(trackList(do.call("list",mget(grep(comp[1],names(show.tracks),value=TRUE))), tracks))$tracks[[5]]$dat$score,
+                                                            optimizeStyle(trackList(do.call("list",mget(grep(comp[2],names(show.tracks),value=TRUE))), tracks))$tracks[[5]]$dat$score))))
+    
+    for(i in 1:5){
+      setTrackStyleParam(u[[i]], "marginBottom", .1)
+    }
+    # For each transcript, the transcript name can be put on the upstream or downstream 
+    #of the transcript
     for(i in 6:length(u)){
-      setTrackStyleParam(u[[i]], "height", 0.04)
+      grx <- genes(TxDb)[as.data.frame(genesinrange)[i-5,1]]
+      if (start(ranges(grx)) <= start(ranges(agr))){
+          setTrackStyleParam(u[[i]], "ylabpos", "left")
+      } 
+      if(end(ranges(grx)) >= end(ranges(agr))){
+        setTrackStyleParam(u[[i]], "ylabpos", "right")
+      } else {
+        setTrackStyleParam(u[[i]], "ylabpos", "upstream")
+      } 
+      setTrackStyleParam(u[[i]], "ylabgp", list(cex=1))
     }
-  } else {
-    setTrackStyleParam(u[[2]], "height", 0.04)
-    for(i in c(1,3:5)){
-      setTrackStyleParam(u[[i]], "height", 0.170-((length(u)-10)*0.01))
+    # Adjust the label of y-axis (colour and size)
+    for (i in 1:5){
+      setTrackStyleParam(u[[i]], "ylabgp", list(cex=.8, col="black")) 
     }
+    # Adjust track colors:
+    setTrackStyleParam(u[[1]], "color", "darkgreen")
+    setTrackStyleParam(u[[2]], "color", "red")
+    setTrackStyleParam(u[[3]], "color", "red")
+    setTrackStyleParam(u[[4]], "color", "purple3")
+    setTrackStyleParam(u[[5]], "color", "darkblue")
     for(i in 6:length(u)){
-      setTrackStyleParam(u[[i]], "height", 0.04)
+      setTrackStyleParam(u[[i]], "color", "grey35")
+    }
+    
+    names(u) <- c("DIFF METH", "DIFF ACT ENH", "H3K27ac", 
+                  "ATAC", "RNA", names(tracks))
+    
+    # Adjust the track height, add conditionals in case many genes are added
+    if (length(u) <= 10){
+      setTrackStyleParam(u[[2]], "height", 0.04)
+      for(i in c(1,3:5)){
+        setTrackStyleParam(u[[i]], "height", 0.170)
+      }
+      for(i in 6:length(u)){
+        setTrackStyleParam(u[[i]], "height", 0.04)
+      }
+    } else {
+      setTrackStyleParam(u[[2]], "height", 0.04)
+      for(i in c(1,3:5)){
+        setTrackStyleParam(u[[i]], "height", 0.170-((length(u)-10)*0.01))
+      }
+      for(i in 6:length(u)){
+        setTrackStyleParam(u[[i]], "height", 0.04)
+      } 
     } 
-  } 
-  
-  
-  if (m == 1){
-    vp <- viewTracks(u, gr=agr, viewerStyle=t, newpage = F)
-  } else {
-    vp <- viewTracks(u, gr=agr, viewerStyle=t, newpage = T)
+    
+    if (m == 1){
+      vp <- viewTracks(u, gr=agr, viewerStyle=t, newpage = F)
+    } else {
+      vp <- viewTracks(u, gr=agr, viewerStyle=t, newpage = T)
+    }
+    addGuideLine(c(start(gr), end(gr)), vp=vp)
+    grid.text(pop[m], x=0.2, y=.9, just="top", 
+              gp=gpar(cex=1.5, fontface="bold"))
   }
-  addGuideLine(c(start(gr), end(gr)), vp=vp)
-  grid.text(pop[m], x=0.2, y=.9, just="top", 
-            gp=gpar(cex=1.5, fontface="bold"))
-  }
-dev.off()
+  dev.off()
 }
