9249607ca5a0b0f5f1e4ff3ef6bca742b021848c
Josep Garnica <61703467+JGarnica22@users.noreply.github.com>
2020-08-27 11:21:39 +0200

    Create Merge_Reads_R.sh

---
+++ RNAseq_analysis_fastq/Merge_Reads_R.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+# Load java module in order to run R
+module purge
+module load gcc/7.2.0 R/3.6.3
+
+# Merge all ReadPerGene dataframe column 3 (stranded or sense aligment) into a unique dataframe for analysis with R, once all jobs are done.
+
+
+R < Merge_Reads.R --save
+
+
+# Remove not needed files
+
+cd mouse
+rm alignment_counts/*SJ.out.tab alignment_counts/*.out.mate1
+rm -r alignment_counts/*_STARgenome alignment_counts/*_STARpass1
+
+for f in $(find ./fastq_files -name "*.fastq.gz" -exec basename {} \;)
+do
+mkdir alignment_counts/$(cut -d'.' -f1 <<< $f)_Logs 
+mv alignment_counts/$(cut -d'.' -f1 <<< $f)*Log*.out alignment_counts/$(cut -d'.' -f1 <<< $f)_Logs
