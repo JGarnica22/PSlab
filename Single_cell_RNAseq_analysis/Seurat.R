6b186ebe061195bc36249c3b69cf704e7b92b8b8
PatriSolé <52314052+patriciasolesanchez@users.noreply.github.com>
2020-09-23 11:34:53 +0200

    Update Seurat.R

--- Single_cell_RNAseq_analysis/Seurat.R
+++ Single_cell_RNAseq_analysis/Seurat.R
@@ -52,7 +52,7 @@ library(cowplot)
 library(magrittr)
 
 # Set your working directory (the project you are working in):
-setwd("C:/Users/jgarn/OneDrive - Universitat de Barcelona/Documentos/Bioinformatics/Functionals/Seurat")
+setwd("/Users/patri/Documents/LAB/TESIS DOCTORAL 2015-2020/TR1 PROJECT/2020_05_10xGenomics_BDC_INS13-21_SANTAMARIA_09/Seurat")
 
 # Specify parameters to be used along the script:
 #Indicate species working with (typicall mouse or human)
@@ -75,15 +75,13 @@ sampletype <- c("BDC_CTL", "BDC_TET", "INS_CTL", "INS_TET")
 
 # Indicate in the Read10X function, the directory of your input files (3 files must be
 # found in the data.dir: matrix.mt.gz, barcodes.tsv.gz and features.tsv.gz)
-data <- Read10X(data.dir = paste0(getwd(), "/data/filtered_feature_bc_matrix/"))
+data <- Read10X(data.dir = paste0(getwd(), "/data/aggr/filtered_feature_bc_matrix/"))
 
 # We next use the count matrix to create a Seurat object. The object serves as a 
 # container that contains both data (like the count matrix) and analysis (like PCA, 
 # or clustering results) for a single-cell dataset.
-    # min.cells: Include features detected in at least this many cells. Will subset the counts matrix as well. 
-    # To reintroduce excluded features, create a new object with a lower cutoff.
-    # min.features: Include cells where at least this many features are detected.
-
+# min.cells: Include features detected in at least this many cells. Will subset the counts matrix as well. 
+# min.features: Include cells where at least this many features are detected.
 scdata <- CreateSeuratObject(counts = data, min.cells = 3, min.features = 200)
 
 # Check the expression (counts) for some genes in the first 100 cells:
@@ -117,10 +115,13 @@ if (species == "mouse") {
 # QC metrics can be found in the Seurat object metadata:
 head(scdata@meta.data)
 
-# Visualize QC metrics as a violin plot
+# Visualize QC metrics as a violin plot:
 VlnPlot(scdata, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), pt.size = 0.5, ncol = 3)
 
-# Subset data (these parameters by default, modify thresholds if you like to filter differently)
+# Subset data (these parameters by default, modify thresholds to filter differently based on your data)
+# QC filtering is performed based on the cell distribution depending on the above mentioned parameters.
+# Check distribution of cells in the Violin plot and keep the main block of cells with a reasonable
+# amount of features detected (usually more than 200), and low mitochondrial contamination. 
 scdata <- subset(scdata, subset = nFeature_RNA>200 & nFeature_RNA<5000 & percent.mt<20)
 
 # Normalization
@@ -162,13 +163,15 @@ DimPlot(scdata, reduction = "umap", dims = c(1,2))
 
 # Clustering
 #In order to find clusters, we use a external function named kmeans
-#This is a particular algorithm that finds cluster for an already defined number of clusters
+#This is a particular algorithm that finds clusters for a predefined number of clusters
 #Define the number of clusters you expect to find:
 numberofclusters <- 3
 #You should perform clustering analysis with different number of clusters and decide which is the
 #correct (real) number of clusters as the last number before the algorithm starts finding clusters
 #that give no information (small group of some cells that are found as a cluster)
 
+# The clustering analysis is perfomed differently every time you run the algorithm. By default we set
+# the seed at 1, but try the analysis different times to ensure the clustering you performed is sensible.
 set.seed(1)
 scdata$kmeans <- kmeans(scdata@reductions[["pca"]]@cell.embeddings, centers = numberofclusters)$cluster
 TSNEPlot(scdata, group.by = "kmeans") + theme(
@@ -215,7 +218,8 @@ ggsave(
 )
 
 # Or you can even split the plot by condition, to see if some cells only appear in one condition:
-TSNEPlot(scdata, split.by = "condition", group.by = "kmeans", 
+# Also you can use shape.by to plot different point shapes for each condition.
+TSNEPlot(scdata, split.by = "condition", group.by = "kmeans",
          pt.size = 0.025) + theme(
            axis.line = element_blank(),
            axis.text.x = element_blank(),
@@ -314,6 +318,8 @@ ggsave(
 # test groups of clusters vs. each other, or against all cells.
 
 # Find markers for every cluster compared to all remaining cells, report only the positive ones
+# By default, FindAllMarkers and FindMarkers function show only genes with a logfc threshold of 0.25 and
+# min.pct of 0.1. If you want to recover more (but less significant) genes, you can modify these thresholds
 Idents(scdata) <- "kmeans"
 markers <- FindAllMarkers(scdata,
                           test.use = "negbinom", min.pct = 0.01)
@@ -322,7 +328,6 @@ write.table(markers, "output/Cluster_markers.txt", quote = F, sep = "\t")
 #You can compare cluster or conditions 1 vs 1:
 Idents(scdata) <- "condition"
 COND <- FindMarkers(scdata, ident.1 = levels(Idents(scdata))[2], ident.2 = levels(Idents(scdata))[1],
-                    verbose = T, test.use = "negbinom",
-                    logfc.threshold = 0, min.pct = 0.01)
+                    verbose = T, test.use = "negbinom")
 
 write.table(COND, "output/Condition_markers.txt", quote = F, sep = "\t")
