10ae730e0b7e28806c2bce7a04ac4cd99691dace
PatriSolé <52314052+patriciasolesanchez@users.noreply.github.com>
2021-01-14 10:36:01 +0100

    Update loop_bowtie2_macs2.sh

--- ATACseq_analysis/loop_bowtie2_macs2.sh
+++ ATACseq_analysis/loop_bowtie2_macs2.sh
@@ -46,10 +46,12 @@ echo cd /gpfs/projects/cek26/ATACseq
 echo java -jar ../software/Trimmomatic-0.39/trimmomatic-0.39.jar SE -threads 64 \
 fastq_files/$f trimmed/trim_$f \
 ILLUMINACLIP:../software/Trimmomatic-0.39/adapters/NexteraSE.fa:2:30:10 LEADING:2 TRAILING:2 SLIDINGWINDOW:4:8 MINLEN:15
-echo bowtie2 -p 32 --very-sensitive -X 2000 -x genome_mus/bowtie2/genome -U trimmed/trim_$f \
+echo bowtie2 -p 32 --very-sensitive -X 2000 -x ../genome_mus/bowtie2/genome -U trimmed/trim_$f \
 \| samtools view -u - \| samtools sort - \> alignment/bowtie_$(cut -d'.' -f1 <<< $f).bam
 echo java -jar ../software/picard.jar MarkDuplicates -I alignment/bowtie_$(cut -d'.' -f1 <<< $f).bam -O alignment/no_dup/nd_$(cut -d'.' -f1 <<< $f).bam \
 -M alignment/no_dup/$(cut -d'.' -f1 <<< $f)_log_dups.txt -REMOVE_DUPLICATES true
+# Code to remove logs from Picard 
+# rm no_dup/*.txt
 echo samtools index alignment/no_dup/nd_$(cut -d'.' -f1 <<< $f).bam
 echo macs2 callpeak -t alignment/no_dup/nd_$(cut -d'.' -f1 <<< $f).bam -f BAM -g 1.87e9 -q 0.05 --nomodel --shift 4 --extsize 150 --keep-dup all \
 -n macs_$(cut -d'.' -f1 <<< $f) --outdir peak_calling 2\> peak_calling/macs_$(cut -d'.' -f1 <<< $f).log
@@ -94,5 +96,6 @@ rm $fastqc/$(cut -d'.' -f1 <<< $f)/*.zip
 done
 convert $fastqc/*.png $fastqc/fastqc_summary.pdf
 
-# Remove used scripts
+# Remove used scripts and log files
 rm -r to_bsub
+rm -r 
