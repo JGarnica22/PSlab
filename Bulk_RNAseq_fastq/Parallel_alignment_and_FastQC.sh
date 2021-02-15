f0ef3ee60ded12f8902efca22b037c5d124c8614
Josep Garnica <61703467+JGarnica22@users.noreply.github.com>
2021-02-15 10:35:58 +0100

    Update Parallel_alignment_and_FastQC.sh

--- Bulk_RNAseq_fastq/Parallel_alignment_and_FastQC.sh
+++ Bulk_RNAseq_fastq/Parallel_alignment_and_FastQC.sh
@@ -4,8 +4,8 @@
 #BSUB -J Loop_fastqc_SA_generate
 #BSUB -q bsc_debug
 #BSUB -W 2:00
-#BSUB -eo /gpfs/projects/cek26/mouse/reports/Loop_fastqc_SA_generate.err
-#BSUB -oo /gpfs/projects/cek26/mouse/reports/Loop_fastqc_SA_generate.out
+#BSUB -eo /gpfs/projects/cek26/project/Loop_fastqc_SA_generate.err
+#BSUB -oo /gpfs/projects/cek26/project/Loop_fastqc_SA_generate.out
 #BSUB -M 1800
 
 
@@ -13,12 +13,15 @@
 module purge
 module load java/1.8.0u66 fastqc intel/2017.4 impi/2017.4 mkl/2017.4 gcc/5.3.0 gcc/4.9.1 openssl/1.1.1c python/3.7.4_pip STAR/2.7.5a
 
+# set working directory
+wd=/gpfs/projects/cek26/project
+
 # Create a folder to store the fastq_files and upload all of them there
 # mkdir fastq_files
 
 # Create folders to store FastQC, STAR alignment, scripts generated and reports. Here state the name you want for your folders
 # If not create a directory for your project
-cd /gpfs/projects/cek26/project
+cd $wd
 fastqc=fastqc
 aligment_counts=aligment_counts
 to_bsub=to_bsub
@@ -40,12 +43,12 @@ for f in $(find ./fastq_files -name "*.fastq.gz" -exec basename {} \;)
 do
 {
 echo \#!/bin/bash
-echo \#BSUB cwd /gpfs/projects/cek26
+echo \#BSUB cwd $wd
 echo \#BSUB -J Aligment_counts_$(cut -d'.' -f1 <<< $f)
 echo \#BSUB -q bsc_ls
 echo \#BSUB -W 2:00
-echo \#BSUB -eo /gpfs/projects/cek26/$bsub_reports/aligment_counts_$(cut -d'.' -f1 <<< $f).err
-echo \#BSUB -oo /gpfs/projects/cek26/bsub_reports/aligment_counts_$(cut -d'.' -f1 <<< $f).out
+echo \#BSUB -eo $wd/$bsub_reports/aligment_counts_$(cut -d'.' -f1 <<< $f).err
+echo \#BSUB -oo $wd/$bsub_reports/aligment_counts_$(cut -d'.' -f1 <<< $f).out
 echo \#BSUB -M 1800
 
 echo module purge
@@ -66,13 +69,13 @@ done
 
 for f in $(find ./fastq_files -name "*.fastq.gz" -exec basename {} \;)
 do
-mkdir fastqc/$(cut -d'.' -f1 <<< $f)  
-fastqc fastq_files/$f --extract -o fastqc/$(cut -d'.' -f1 <<< $f)
-fastqc/$(cut -d'.' -f1 <<< $f)/$(cut -d'.' -f1 <<< $f)_fastqc/summary.txt | \
-	montage txt:- fastqc/$(cut -d'.' -f1 <<< $f)/$(cut -d'.' -f1 <<< $f)_fastqc/Images/*.png \
-	-tile x3 -geometry +0.1+0.1 -title $(cut -d'.' -f1 <<< $f) fastqc/$(cut -d'.' -f1 <<< $f).png
-rm fastqc/$(cut -d'.' -f1 <<< $f)/*.zip
+mkdir $fastqc/$(cut -d'.' -f1 <<< $f)  
+fastqc $fastq_files/$f --extract -o $fastqc/$(cut -d'.' -f1 <<< $f)
+$fastqc/$(cut -d'.' -f1 <<< $f)/$(cut -d'.' -f1 <<< $f)_fastqc/summary.txt | \
+	montage txt:- $fastqc/$(cut -d'.' -f1 <<< $f)/$(cut -d'.' -f1 <<< $f)_fastqc/Images/*.png \
+	-tile x3 -geometry +0.1+0.1 -title $(cut -d'.' -f1 <<< $f) $fastqc/$(cut -d'.' -f1 <<< $f).png
+rm $fastqc/$(cut -d'.' -f1 <<< $f)/*.zip
 done
 
 # Create a pdf containing all images for all samples as a report
-convert fastqc/*.png fastqc/fastqc_summary.pdf
+convert $fastqc/*.png $fastqc/fastqc_summary.pdf
