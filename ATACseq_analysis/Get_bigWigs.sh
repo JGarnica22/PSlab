1165dda6c82cfcac145680d25f59c10880bd4479
Josep Garnica <61703467+JGarnica22@users.noreply.github.com>
2021-11-18 16:25:31 +0100

    Create Get_bigWigs.sh

---
+++ ATACseq_analysis/Get_bigWigs.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#BSUB cwd /gpfs/projects/cek26
+#BSUB -J bamcoverage
+#BSUB -q bsc_debug
+#BSUB -W 4:00
+#BSUB -eo /gpfs/projects/cek26/experiments/SANTAMARIA_31/bsub_reports/bamcoverage.err
+#BSUB -oo /gpfs/projects/cek26/experiments/SANTAMARIA_31/bsub_reports/bamcoverage.out
+#BSUB -M 1800
+#BSUB -n 16
+#BSUB -R "span[ptile=16]"
+#BSUB -x
+
+
+###############################################################################################
+# THIS SCRIPT GENERATE .bw FILES OUT OF .bam FILES USING THE TOOLS bamCoverage from DEEPTOOLS
+# .bw FILES CAN BE THEN USED FOR GENOMIC VIEWERS UTILITIES
+################################################################################################
+
+
+
+# Set your working directory
+wd=/gpfs/projects/cek26/experiments/SANTAMARIA_31/alignment/no_dup
+cd $wd
+mkdir bw
+
+## indexing already done (.bai files present)
+mkdir bw
+for f in $(find . -name "*.bam" -exec basename {} \;)
+do
+	echo doing bamCoverage on $f
+	bamCoverage -p 16 -b $f -o bw/coverage_$(cut -d'.' -f1 <<< $f).bw 
+	echo coverage_$(cut -d'.' -f1 <<< $f).bw file created
+done
+
+
+## indexing not done
+for f in $(find . -name "*.bam" -exec basename {} \;)
+do
+	echo "Indexing:"$f
+	samtools index @ 16 $f
+	echo $f".bai index file created"
+	echo doing bamCoverage on $f
+	bamCoverage -p 16 -b $f -o Coverage_$(cut -d'.' -f1 <<< $f).bw
+	echo Coverage_$(cut -d'.' -f1 <<< $f).bw file created
+done
