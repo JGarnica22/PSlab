199bc130ef9bd27170b1003a9fd781bab7de6b40
JoelMoro <62171295+JoelMoro@users.noreply.github.com>
2022-01-31 16:15:19 +0100

    Update sbatch_header_low.sh

--- HowTo's/sbatch_header_low.sh
+++ HowTo's/sbatch_header_low.sh
@@ -5,7 +5,7 @@
 #SBATCH -t 2:00:00
 #SBATCH -e /gpfs/projects/cek26/experiments/test.err
 #SBATCH -o /gpfs/projects/cek26/experiments/test.out
-#SBATCH -n 64
+#SBATCH -n 16
 #SBATCH --mail-type=all
 #SBATCH --mail-user=jmorobor@gmail.com
 
