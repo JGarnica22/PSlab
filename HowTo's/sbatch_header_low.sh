fd0c444bba297c0cc7f98c62c8a2fa13103fda66
JoelMoro <62171295+JoelMoro@users.noreply.github.com>
2022-01-31 16:11:18 +0100

    Create sbatch_header_low.sh

---
+++ HowTo's/sbatch_header_low.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -D /gpfs/projects/cek26
+#SBATCH --job-name="hello_test"
+#SBATCH -q debug
+#SBATCH -t 2:00:00
+#SBATCH -e /gpfs/projects/cek26/experiments/test.err
+#SBATCH -o /gpfs/projects/cek26/experiments/test.out
+#SBATCH -n 64
+#SBATCH --mail-type=all
+#SBATCH --mail-user=jmorobor@gmail.com
+
+
+echo Trying Slurm
