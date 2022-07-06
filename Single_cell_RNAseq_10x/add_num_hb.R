faa99301c8dadc685a5a162f833313709c323cdd
Josep Garnica <61703467+JGarnica22@users.noreply.github.com>
2022-07-06 09:31:13 +0200

    Update add_num_hb.R

--- Single_cell_RNAseq_10x/add_num_hb.R
+++ Single_cell_RNAseq_10x/add_num_hb.R
@@ -1,21 +1,22 @@
+
 # li is a rowbinded dataframe with all comparisons (used for HB plot)
 # comparison col would be TFH_vs_TR1.2 etc
-# pathway_type col: cell cycle, mitosis, ribosomal...
+# comparison the type of pathway: cell cycle, mitosis, ribosomal...
 
 fc <- 0.25 # log2FC threeshold
 lol <- table(li2$avg_log2FC>=fc, li2$comparison, li2$pathway_type) %>%
   as.data.frame() %>% filter(Var1 == TRUE) %>% rename(Var1="UP")
 lol2 <- table(li2$avg_log2FC <= -fc, li2$comparison, li2$pathway_type) %>%
   as.data.frame() %>% filter(Var1 == TRUE) %>% rename(Var1="DOWN")
-lol3 <- rbind(lol2, lol) %>% arrange(Var3, desc(Var2))
+lol3 <- rbind(lol2, lol)%>% arrange(Var3, desc(Var2))
 
 #dataframe for positions
 pt <- c()
-l <- length(unique(lol3$comparison)
+l <- length(unique(lol3$comparison))
 mi <- min(li2)*1.1
 max <- max(li2)*1.1
-for(i in unique(lol3$comparison){
-  pt <- append(pt,c(rep(i,l))))
+for(i in unique(lol3$comparison)){
+  pt <- append(pt,c(rep(i,l)))
 }
 dat_text <- data.frame(
   label= as.character(lol3$Freq),
@@ -32,3 +33,4 @@ hb2 <- hb + geom_text(
                       size= 8.5,
                       show.legend = F
                       )
+
